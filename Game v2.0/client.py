56f86cbe77dcbf9392a22f75309713961452916e
yandex-lyceum-yakovlev <56777612+yandex-lyceum-yakovlev@users.noreply.github.com>
2020-01-01 19:52:27 +0400

    Game v2.0

---
+++ Game v2.0/client.py
@@ -0,0 +1,105 @@
+﻿from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import pygame
+import pickle
+import sys
+import random
+
+
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZE)
+            try:
+                server_players = pickle.loads(msg)
+                for pid in server_players:
+                    if pid not in players:
+                        Player(all_sprites, pid)
+                    players[pid] = server_players[pid]
+            except pickle.UnpicklingError:
+                try:
+                    t = msg.decode("utf8")
+                    if t.startswith("{quit}"):
+                        client_socket.close()
+                        break
+                    if t.startswith("del"):
+                        del_id = t.split()[1]
+                        del players[del_id]
+                except UnicodeDecodeError:
+                    continue
+        except OSError:  # Possibly client has left the chat.
+            break
+
+
+class Player(pygame.sprite.Sprite):
+    enemy_image = pygame.image.load("data/bomb.png")
+    my_image = pygame.image.load("data/bomb2.png")
+
+    def __init__(self, group, id):
+        super().__init__(group)
+        if id != player_id:
+            self.image = Player.enemy_image
+        else:
+            self.image = Player.my_image
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+        self.id = id
+
+    def update(self, *args):
+        if self.id not in players:
+            self.kill()
+        else:
+            self.rect.center = players[self.id]
+
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+
+running = True
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+
+v = 10
+shift = {pygame.K_LEFT: (-v, 0), pygame.K_RIGHT: (v, 0),
+         pygame.K_UP: (0, -v), pygame.K_DOWN: (0, v)}
+bot = False
+
+HOST = "127.0.0.1"  # input('Enter host: ')
+PORT = 33000  # input('Enter port: ')
+if len(sys.argv) > 1:
+    HOST = sys.argv[1]
+if len(sys.argv) > 2:
+    PORT = int(sys.argv[2])
+if len(sys.argv) > 3:
+    bot = sys.argv[3] == "bot"
+BUFSIZE = 1024
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+receive_thread = Thread(target=receive)
+receive_thread.start()
+
+player_id = str(random.randint(1, 1000000))
+player_name = "player" + player_id  # input('Введите ник игрока')
+client_socket.send(bytes(f"{player_id} {player_name}", "utf8"))
+players = {}
+
+bot_event = 30
+if bot:
+    pygame.time.set_timer(bot_event, 100)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            client_socket.send(bytes("{quit}", "utf8"))
+            running = False
+        if event.type == bot_event:
+            client_socket.send(pickle.dumps(random.choice(list(shift.values()))))
+    for s in shift:
+        if pygame.key.get_pressed()[s]:
+            client_socket.send(pickle.dumps(shift[s]))
+    screen.fill(pygame.Color("white"))
+    all_sprites.update()
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(100)
---
+++ Game v2.0/server.py
@@ -0,0 +1,61 @@
+﻿from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import pickle
+
+
+def accept_incoming_connections():  # новое подключение
+    while True:
+        client, client_address = SERVER.accept()
+        print(f"Новый игрок {client_address}")
+        Thread(target=handle_client, args=(client,)).start()
+
+
+def handle_client(client):  # Takes client socket as argument.
+    player_id, player_name = client.recv(BUFSIZE).decode("utf8").split()
+    clients[client] = (player_id, player_name)
+    positions[player_id] = (300, 300)
+    broadcast(pickle.dumps(positions))
+
+    while True:
+        try:
+            msg = client.recv(BUFSIZE)
+            if msg != bytes("{quit}", "utf8"):
+                s = pickle.loads(msg)
+                old = positions[player_id]
+                positions[player_id] = ((old[0] + s[0]) % 600, (old[1] + s[1]) % 600)
+                print(positions)
+                broadcast(pickle.dumps(positions))
+            else:
+                print(f"{player_name} вышел из игры")
+                client.send(bytes("{quit}", "utf8"))
+                client.close()
+                del clients[client]
+                del positions[player_id]
+                broadcast(bytes(f"del {player_id}", "utf8"))
+                break
+        except ConnectionResetError:
+            break
+
+
+def broadcast(msg):  # разослать сообщение всем клиентам
+    for sock in clients:
+        sock.send(msg)
+
+
+clients = {}
+positions = {}
+
+HOST = ''
+PORT = 33000
+BUFSIZE = 1024
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind((HOST, PORT))
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Ждем подключения...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
